bfac15a975f7a80b6fd188514365bd6456b5a822
Bernd Verst <github@bernd.dev>
2024-05-02 18:08:43 -0700

    Update protos
    Signed-off-by: Bernd Verst <github@bernd.dev>

--- backend/azure/durabletaskservice/internal/backend/v1/events.pb.go
+++ backend/azure/durabletaskservice/internal/backend/v1/events.pb.go
@@ -899,7 +899,7 @@ type SubOrchestrationInstanceCompletedEvent struct {
 	sizeCache     protoimpl.SizeCache
 	unknownFields protoimpl.UnknownFields
 
-	// Sequence number of the event that scheduled this activity
+	// Sequence number of the event that scheduled this work item
 	RelatedSequenceNumber int64  `protobuf:"varint,1,opt,name=related_sequence_number,json=relatedSequenceNumber,proto3" json:"related_sequence_number,omitempty"`
 	Result                []byte `protobuf:"bytes,2,opt,name=result,proto3" json:"result,omitempty"`
 }
@@ -956,7 +956,7 @@ type SubOrchestrationInstanceFailedEvent struct {
 	sizeCache     protoimpl.SizeCache
 	unknownFields protoimpl.UnknownFields
 
-	// Sequence number of the event that scheduled this activity
+	// Sequence number of the event that scheduled this work item
 	RelatedSequenceNumber int64           `protobuf:"varint,1,opt,name=related_sequence_number,json=relatedSequenceNumber,proto3" json:"related_sequence_number,omitempty"`
 	Result                []byte          `protobuf:"bytes,2,opt,name=result,proto3" json:"result,omitempty"`
 	FailureDetails        *FailureDetails `protobuf:"bytes,3,opt,name=failure_details,json=failureDetails,proto3" json:"failure_details,omitempty"`
@@ -1022,7 +1022,7 @@ type TimerCreatedEvent struct {
 	unknownFields protoimpl.UnknownFields
 
 	// Scheduled execution time for the timer
-	ExecutionTime *timestamppb.Timestamp `protobuf:"bytes,5,opt,name=execution_time,json=executionTime,proto3" json:"execution_time,omitempty"`
+	StartAt *Delay `protobuf:"bytes,1,opt,name=start_at,json=startAt,proto3" json:"start_at,omitempty"`
 }
 
 func (x *TimerCreatedEvent) Reset() {
@@ -1057,9 +1057,9 @@ func (*TimerCreatedEvent) Descriptor() ([]byte, []int) {
 	return file_backend_v1_events_proto_rawDescGZIP(), []int{10}
 }
 
-func (x *TimerCreatedEvent) GetExecutionTime() *timestamppb.Timestamp {
+func (x *TimerCreatedEvent) GetStartAt() *Delay {
 	if x != nil {
-		return x.ExecutionTime
+		return x.StartAt
 	}
 	return nil
 }
@@ -1070,7 +1070,7 @@ type TimerExecutedEvent struct {
 	sizeCache     protoimpl.SizeCache
 	unknownFields protoimpl.UnknownFields
 
-	// Sequence number of the event that scheduled this activity
+	// Sequence number of the event that scheduled this work item
 	RelatedSequenceNumber int64 `protobuf:"varint,1,opt,name=related_sequence_number,json=relatedSequenceNumber,proto3" json:"related_sequence_number,omitempty"`
 }
 
@@ -1802,12 +1802,11 @@ var file_backend_v1_events_proto_rawDesc = []byte{
 	0x03, 0x20, 0x01, 0x28, 0x0b, 0x32, 0x1f, 0x2e, 0x64, 0x74, 0x6d, 0x62, 0x2e, 0x62, 0x61, 0x63,
 	0x6b, 0x65, 0x6e, 0x64, 0x2e, 0x76, 0x31, 0x2e, 0x46, 0x61, 0x69, 0x6c, 0x75, 0x72, 0x65, 0x44,
 	0x65, 0x74, 0x61, 0x69, 0x6c, 0x73, 0x52, 0x0e, 0x66, 0x61, 0x69, 0x6c, 0x75, 0x72, 0x65, 0x44,
-	0x65, 0x74, 0x61, 0x69, 0x6c, 0x73, 0x22, 0x56, 0x0a, 0x11, 0x54, 0x69, 0x6d, 0x65, 0x72, 0x43,
-	0x72, 0x65, 0x61, 0x74, 0x65, 0x64, 0x45, 0x76, 0x65, 0x6e, 0x74, 0x12, 0x41, 0x0a, 0x0e, 0x65,
-	0x78, 0x65, 0x63, 0x75, 0x74, 0x69, 0x6f, 0x6e, 0x5f, 0x74, 0x69, 0x6d, 0x65, 0x18, 0x05, 0x20,
-	0x01, 0x28, 0x0b, 0x32, 0x1a, 0x2e, 0x67, 0x6f, 0x6f, 0x67, 0x6c, 0x65, 0x2e, 0x70, 0x72, 0x6f,
-	0x74, 0x6f, 0x62, 0x75, 0x66, 0x2e, 0x54, 0x69, 0x6d, 0x65, 0x73, 0x74, 0x61, 0x6d, 0x70, 0x52,
-	0x0d, 0x65, 0x78, 0x65, 0x63, 0x75, 0x74, 0x69, 0x6f, 0x6e, 0x54, 0x69, 0x6d, 0x65, 0x22, 0x4c,
+	0x65, 0x74, 0x61, 0x69, 0x6c, 0x73, 0x22, 0x46, 0x0a, 0x11, 0x54, 0x69, 0x6d, 0x65, 0x72, 0x43,
+	0x72, 0x65, 0x61, 0x74, 0x65, 0x64, 0x45, 0x76, 0x65, 0x6e, 0x74, 0x12, 0x31, 0x0a, 0x08, 0x73,
+	0x74, 0x61, 0x72, 0x74, 0x5f, 0x61, 0x74, 0x18, 0x01, 0x20, 0x01, 0x28, 0x0b, 0x32, 0x16, 0x2e,
+	0x64, 0x74, 0x6d, 0x62, 0x2e, 0x62, 0x61, 0x63, 0x6b, 0x65, 0x6e, 0x64, 0x2e, 0x76, 0x31, 0x2e,
+	0x44, 0x65, 0x6c, 0x61, 0x79, 0x52, 0x07, 0x73, 0x74, 0x61, 0x72, 0x74, 0x41, 0x74, 0x22, 0x4c,
 	0x0a, 0x12, 0x54, 0x69, 0x6d, 0x65, 0x72, 0x45, 0x78, 0x65, 0x63, 0x75, 0x74, 0x65, 0x64, 0x45,
 	0x76, 0x65, 0x6e, 0x74, 0x12, 0x36, 0x0a, 0x17, 0x72, 0x65, 0x6c, 0x61, 0x74, 0x65, 0x64, 0x5f,
 	0x73, 0x65, 0x71, 0x75, 0x65, 0x6e, 0x63, 0x65, 0x5f, 0x6e, 0x75, 0x6d, 0x62, 0x65, 0x72, 0x18,
@@ -1894,6 +1893,7 @@ var file_backend_v1_events_proto_goTypes = []interface{}{
 	(*TraceContext)(nil),                           // 23: dtmb.backend.v1.TraceContext
 	(OrchestrationStatus)(0),                       // 24: dtmb.backend.v1.OrchestrationStatus
 	(*FailureDetails)(nil),                         // 25: dtmb.backend.v1.FailureDetails
+	(*Delay)(nil),                                  // 26: dtmb.backend.v1.Delay
 }
 var file_backend_v1_events_proto_depIdxs = []int32{
 	21, // 0: dtmb.backend.v1.Event.timestamp:type_name -> google.protobuf.Timestamp
@@ -1926,7 +1926,7 @@ var file_backend_v1_events_proto_depIdxs = []int32{
 	25, // 27: dtmb.backend.v1.ActivityFailedEvent.failure_details:type_name -> dtmb.backend.v1.FailureDetails
 	23, // 28: dtmb.backend.v1.SubOrchestrationInstanceCreatedEvent.parent_trace_context:type_name -> dtmb.backend.v1.TraceContext
 	25, // 29: dtmb.backend.v1.SubOrchestrationInstanceFailedEvent.failure_details:type_name -> dtmb.backend.v1.FailureDetails
-	21, // 30: dtmb.backend.v1.TimerCreatedEvent.execution_time:type_name -> google.protobuf.Timestamp
+	26, // 30: dtmb.backend.v1.TimerCreatedEvent.start_at:type_name -> dtmb.backend.v1.Delay
 	24, // 31: dtmb.backend.v1.HistoryStateEvent.orchestration_status:type_name -> dtmb.backend.v1.OrchestrationStatus
 	32, // [32:32] is the sub-list for method output_type
 	32, // [32:32] is the sub-list for method input_type
