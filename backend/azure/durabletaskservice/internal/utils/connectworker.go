9f764778874560d808fc0dc61f3e5477bb1a4eac
ItalyPaleAle <43508+ItalyPaleAle@users.noreply.github.com>
2024-05-21 07:55:05 -0700

    Add support for reconnecting

--- backend/azure/durabletaskservice/internal/utils/connectworker.go
+++ backend/azure/durabletaskservice/internal/utils/connectworker.go
@@ -9,6 +9,7 @@ import (
 	"io"
 	"log"
 	"strings"
+	"sync/atomic"
 	"time"
 
 	"github.com/microsoft/durabletask-go/backend/azure/durabletaskservice/internal/backend/v1"
@@ -29,6 +30,8 @@ func ConnectWorker(
 	clientMsgChan <-chan *backend.ConnectWorkerClientMessage,
 	readyCb func(),
 ) error {
+	retryClientMsgChan := make(chan *backend.ConnectWorkerClientMessage, 1)
+	defer close(retryClientMsgChan)
 	defer close(serverMsgChan)
 
 	// Stream ID used for correlating debug logs
@@ -42,33 +45,112 @@ func ConnectWorker(
 		streamID = hex.EncodeToString(streamIDBytes)
 	}
 
+	// Define the establishWorkerConnection message
+	establishWorkerConnectionMsg := &backend.EstablishWorkerConnectionMessage{
+		Version:              UserAgent,
+		ActivityFunction:     activityFns.ToProto(),
+		OrchestratorFunction: orchestratorFns.ToProto(),
+	}
+
+	// readyCb can only be invoked once
+	var readyCbInvoked *atomic.Bool
+	if readyCb != nil {
+		readyCbInvoked = &atomic.Bool{}
+		readyCbOrig := readyCb
+		readyCb = func() {
+			readyCbInvoked.Store(true)
+			readyCbOrig()
+		}
+	}
+
+	// We retry connecting until we get a persistent error
+	for {
+		log.Printf("[%s] Establishing connection with service", streamID)
+
+		// Invoke the connectWorkerInternal method to perform the rest of the work
+		workerConfig, err := connectWorkerInternal(
+			ctx, streamID,
+			taskHub, worker,
+			establishWorkerConnectionMsg,
+			serverMsgChan, clientMsgChan, retryClientMsgChan,
+			readyCb,
+		)
+
+		// Check if we have a persistent error
+		switch {
+		case ctx.Err() != nil:
+			// Our context has been canceled, this is persistent
+			return fmt.Errorf("closing stream: %w", ctx.Err())
+
+		case errors.Is(err, io.EOF):
+			// Indicates that the server closed the connection gracefully
+			// The instance of the service was shutting down gracefully
+			log.Printf("[%s] Connection closed by the server", streamID)
+
+		case err != nil:
+			log.Printf("[%s] Error from ConnectWorker stream: %v", streamID, err)
+
+		default:
+			log.Printf("[%s] ConnectWorker returned with no error", streamID)
+		}
+
+		// If we have a workerConfig, update the worker ID in the EstablishWorkerConnection message
+		if workerConfig != nil && workerConfig.WorkerId != "" {
+			establishWorkerConnectionMsg.WorkerId = workerConfig.WorkerId
+		}
+
+		// readyCb cannot be invoked more than once
+		if readyCb != nil && readyCbInvoked.Load() {
+			readyCb = nil
+		}
+
+		// Sleep before retrying
+		select {
+		case <-time.After(time.Second):
+			// All good
+		case <-ctx.Done():
+			return fmt.Errorf("closing stream: %w", ctx.Err())
+		}
+	}
+}
+
+func connectWorkerInternal(
+	ctx context.Context,
+	streamID string,
+	taskHub string,
+	worker backend.TaskHubWorkerClient,
+	establishWorkerConnectionMsg *backend.EstablishWorkerConnectionMessage,
+	serverMsgChan chan<- *backend.ConnectWorkerServerMessage,
+	clientMsgChan <-chan *backend.ConnectWorkerClientMessage,
+	retryClientMsgChan chan *backend.ConnectWorkerClientMessage,
+	readyCb func(),
+) (wc *backend.WorkerConfiguration, err error) {
+	// Replace the context with one that we can cancel when this method returns
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
 	// Establish the ConnectWorker stream
 	// streamCtx := metadata.AppendToOutgoingContext(ctx,
 	// 	"taskhub", taskHub,
 	// )
 	stream, err := worker.ConnectWorker(ctx)
 	if err != nil {
-		return fmt.Errorf("error starting ConnectWorker: %w", err)
+		return nil, fmt.Errorf("error starting ConnectWorker: %w", err)
 	}
 
 	// Send the initial message
 	err = stream.Send(&backend.ConnectWorkerClientMessage{
 		Message: &backend.ConnectWorkerClientMessage_EstablishWorkerConnection{
-			EstablishWorkerConnection: &backend.EstablishWorkerConnectionMessage{
-				Version:              UserAgent,
-				ActivityFunction:     activityFns.ToProto(),
-				OrchestratorFunction: orchestratorFns.ToProto(),
-			},
+			EstablishWorkerConnection: establishWorkerConnectionMsg,
 		},
 	})
 	if err != nil {
-		return fmt.Errorf("error sending EstablishWorkerConnectionMessage message: %w", err)
+		return nil, fmt.Errorf("error sending EstablishWorkerConnectionMessage message: %w", err)
 	}
 
 	// Wait for the first message
 	timeout := time.NewTimer(5 * time.Second)
 	configReceived := make(chan error)
-	var wc *backend.WorkerConfiguration
 	go func() {
 		msg, err := stream.Recv()
 		if err != nil {
@@ -94,10 +176,10 @@ func ConnectWorker(
 			<-timeout.C
 		}
 		if err != nil {
-			return fmt.Errorf("error receiving configuration: %w", err)
+			return nil, fmt.Errorf("error receiving configuration: %w", err)
 		}
 	case <-timeout.C:
-		return errors.New("timed out waiting for configuration message")
+		return nil, errors.New("timed out waiting for configuration message")
 	}
 
 	// We are ready
@@ -190,14 +272,8 @@ func ConnectWorker(
 	for {
 		select {
 		case err = <-errChan:
-			// io.EOF means the stream was closed by the server, so we can return cleanly
-			if errors.Is(err, io.EOF) {
-				log.Printf("[%s] Stream ended…", streamID)
-				return nil
-			}
-
 			// We have an error; return
-			return err
+			return wc, err
 
 		case msg := <-msgChan:
 			// Reset healthCheckTick
@@ -216,7 +292,7 @@ func ConnectWorker(
 		case <-healthCheckTick.C:
 			// A signal on healthCheckTick indicates that we haven't received a message from the server in an amount of time
 			// Assume the server is dead
-			return fmt.Errorf("did not receive a message from the server in %v; closing connection", healthCheckDuration)
+			return wc, fmt.Errorf("did not receive a message from the server in %v; closing connection", healthCheckDuration)
 		}
 	}
 }
